e814b508dec1d8ea02a7e0a8308e7357d12d734d
Roberto Ciccarelli <robyciccarelli.rc@gmail.com>
2024-05-30 20:13:29 +0200

    Reorder code / 1

--- src/graphics.jl
+++ src/graphics.jl
@@ -1,16 +1,32 @@
 
 tolatex(x::Float64) = LaTeXString(replace(string(round(x, sigdigits=6)), r"e(-?\d+)" => s"\\times 10^{\1}") )
 
+function register_click_interaction!(ax, t)
+    register_interaction!(ax, :click_on_plot) do event::MouseEvent, axis
+        if event.type === MouseEventTypes.leftclick
+            t[] = round(event.data[1])
+        end
+    end
+end
 
 function path_animation(Γ::Coefficients; period = 12.0, nsteps = 100)
     
     path = symmetrize(build_path(Γ, nsteps))
+
+    l = length(path)-1
+
+    fps = length(path)/period
+
     
+    action_value = tolatex(action(Γ))
+    ∇action_norm = tolatex(norm(∇action(Γ)))
+
+    E =  symmetrize(K_energy(Γ, nsteps) - U(Γ, nsteps))
+    meanE = sum(E)/length(E)
+
     theme = merge(theme_dark(), theme_latexfonts())
     set_theme!(theme)
 
-    action_value = tolatex(action(flatten(Γ)))
-    ∇action_norm = tolatex(norm(∇action(flatten(Γ))))
 
     f = Figure(size=(1500, 800), color=:black, title = "My Custom Window Title")
     al = AmbientLight(RGBf(0.3, 0.3, 0.3))
@@ -22,46 +38,24 @@ function path_animation(Γ::Coefficients; period = 12.0, nsteps = 100)
     grid = GridLayout()
     f.layout[1,2] = grid
 
-    padding_left = (8.0f0, 8.0f0, 4.0f0, 12.0f0)
-    padding_right = (8.0f0, 8.0f0, 4.0f0, 12.0f0)
+    padding = (8.0f0, 8.0f0, 4.0f0, 12.0f0)
 
-    Label(grid[1, 1], "Action:", fontsize=24, color=:white,halign=:left, padding=padding_left)
-    Label(grid[1, 2],  action_value, halign=:left, fontsize=24, color=:white, padding=padding_right)
+    Label(grid[1, 1], "Action:", fontsize=24, color=:white,halign=:left, padding=padding)
+    Label(grid[1, 2],  action_value, halign=:left, fontsize=24, color=:white, padding=padding)
 
 
-    Label(grid[2, 1], L"||\nabla \text{Action}||:", fontsize=24, halign=:left, color=:white, padding=padding_left)
-    Label(grid[2, 2],  ∇action_norm, halign=:left, fontsize=24, color=:white, padding=padding_right)
-
+    Label(grid[2, 1], L"||\nabla \text{Action}||:", fontsize=24, halign=:left, color=:white, padding=padding)
+    Label(grid[2, 2],  ∇action_norm, halign=:left, fontsize=24, color=:white, padding=padding)
+    
     colsize!(f.layout, 2, Relative(1/3))
 
-    
-    l = length(path)-1
-    
-    for i ∈ 1:N
-        lines!(scene, 
-        [[path[j][i][h] for j ∈ 0:l] for h ∈ 1:dim]..., 
-        color=i, colormap=:lightrainbow, colorrange = (1,N), label="Body $i", transparency=true, ssao=true, fxaa=true, linewidth=2);
-    end
-    
     ax = Axis(f[4,2], title="Distance between bodies", ylabel = L"r_{ij}", xticklabelcolor=:white, yticklabelcolor=:white, ylabelcolor=:white, titlecolor=:white, titlesize=22, xzoomlock=true, yzoomlock=true, yrectzoom=false,xrectzoom=false)
-    for i ∈ 1:N-1, j ∈ i+1:N
-        r_ij = [norm(path[k][i] - path[k][j]) for k ∈ 0:l]
-        lines!(ax, 0:(length(r_ij)-1), r_ij, label=L"r_{%$i%$j}")
-    end
-    
     ax2 = Axis(f[3,2],  title="Energy", titlecolor=:white, xticklabelcolor=:white, yticklabelcolor=:white, ylabel=L"E", ylabelcolor=:white,  titlesize=22, xzoomlock=true, yzoomlock=true, yrectzoom=false,xrectzoom=false)
 
-    E =  symmetrize(K_energy(Γ, nsteps) - U(Γ, nsteps))
-    meanE = sum(E)/length(E)
+    t = Observable(0)
 
-    lines!(ax2, 0:(length(E) -1), OffsetArrays.no_offset_view(E), label="K Energy", linewidth=2)
-
-    ylims!(ax2, min(E..., 0.8*meanE, 1.2*meanE), max(E..., 1.2*meanE, 0.8*meanE))
-
-    Legend(f[5,2], ax,  orientation = :horizontal, labelcolor=:white, labelsize=24)
-    axislegend(scene,  labelcolor=:white, labelsize=18, padding=(6.0f0, 12.0f0, 6.0f0, 6.0f0))
-    
-    fps = length(path)/period
+    register_click_interaction!(ax, t)
+    register_click_interaction!(ax2, t)
 
     if dim == 2
         bodies = Observable([Point2(zeros(2)) for _ ∈ 1:N])
@@ -70,13 +64,28 @@ function path_animation(Γ::Coefficients; period = 12.0, nsteps = 100)
     else 
         return
     end
+    
+    for i ∈ 1:N
+        lines!(scene, 
+                [[path[j][i][h] for j ∈ 0:l] for h ∈ 1:dim]..., 
+                color=i, colormap=:lightrainbow, colorrange = (1,N), 
+                label="Body $i", transparency=true, ssao=true, fxaa=true, linewidth=2
+            )
+    end
+    
+    for i ∈ 1:N-1, j ∈ i+1:N
+        r_ij = [norm(path[k][i] - path[k][j]) for k ∈ 0:l]
+        lines!(ax, 0:l, r_ij, label=L"r_{%$i%$j}")
+    end
+       
+    lines!(ax2, 0:l, OffsetArrays.no_offset_view(E), label="K Energy", linewidth=2)
 
+    ylims!(ax2, min(E..., 0.8*meanE, 1.2*meanE), max(E..., 1.2*meanE, 0.8*meanE))
 
+    Legend(f[5,2], ax,  orientation = :horizontal, labelcolor=:white, labelsize=24)
+    axislegend(scene,  labelcolor=:white, labelsize=18, padding=(6.0f0, 12.0f0, 6.0f0, 6.0f0))
+    
     meshscatter!(scene, bodies, color=1:N, colormap=:lightrainbow, colorrange=(1,N), markersize = 0.05)
-    t = 0
-    t = Observable(0)
-    vlines!(ax, t, color=:red)
-    vlines!(ax2, t, color=:red)
 
 
     sliders = SliderGrid(f[2, 2], 
@@ -95,22 +104,12 @@ function path_animation(Γ::Coefficients; period = 12.0, nsteps = 100)
     sliders.valuelabels[1].color[] = RGBf(1, 1, 1)
     sliders.labels[1].fontsize[] = 20
     sliders.valuelabels[1].fontsize[] = 20
-       
+
+    vlines!(ax, t, color=:red)
+    vlines!(ax2, t, color=:red)
+
+    
     display(f,  title = "Orbit Animation")
-
-    i = Observable(0)
-    register_interaction!(ax, :click_on_plot) do event::MouseEvent, axis
-        if event.type === MouseEventTypes.leftclick
-            t[] = round(event.data[1])
-        end
-    end
-
-    register_interaction!(ax2, :click_on_plot) do event::MouseEvent, axis
-        if event.type === MouseEventTypes.leftclick
-            t[] = round(event.data[1])
-        end
-    end
-
   
     while true
         if ! events(scene).window_open[] return end
@@ -120,7 +119,6 @@ function path_animation(Γ::Coefficients; period = 12.0, nsteps = 100)
         fps = length(path)/period*10^(sliders.sliders[1].value[])
         sleep(1.0/fps)
     end
-
 
 end
 
